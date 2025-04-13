69b3aae515bd3421b8d588942da2b542e5be8d6d
SIDDHARTHGUPTA <contactsid14@gmail.com>
2025-04-13 18:05:34 +0530

    middleware function for upload

---
+++ backend/middlewares/uploadMiddleware.js
@@ -0,0 +1,22 @@
+const multer = require('multer');
+const AppError = require('../utils/appError');
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+  limits: {
+    fileSize: 5 * 1024 * 1024 // 5MB
+  }
+});
+
+exports.uploadUserPhoto = upload.single('photo');
